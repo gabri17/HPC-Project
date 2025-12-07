17ae50f10d9d6c83f766f706baa61806b9c45a30
Sano <sanasar.hambardzumyan@gmail.com>
2025-12-07 18:49:31 +0100

    feat: add script for running buffered romberg_1D and romberg_2D

---
+++ romberg_1d_2d/run_romberg_2D.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -l select=2:ncpus=8:mem=2gb
+#PBS -l walltime=0:05:00
+#PBS -q short_cpuQ
+#PBS -N RombergTest
+#PBS -V
+
+module load mpich-3.2
+cd $PBS_O_WORKDIR
+mpirun.actual -n 16 ./romberg_2D
