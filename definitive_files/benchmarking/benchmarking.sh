dfbf9194bdc76f58668698a273be5d1c8178375e
Volani <gabriele.volani@hpc-head-n2.unitn.it>
2025-12-30 10:31:05 +0100

    new benchmarking

--- definitive_files/benchmarking/benchmarking.sh
+++ definitive_files/benchmarking/benchmarking.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#PBS -l select=64:ncpus=4:mem=2gb
-#PBS -l walltime=0:20:00
-#PBS -q short_cpuQ
+#PBS -l select=1:ncpus=1:mem=2gb -l place=pack:excl
+#PBS -l walltime=0:30:00
+#PBS -q short_HPC4DS
 #PBS -V
 
 # Change to the folder where job is submitted (where the executable is located)
 cd $PBS_O_WORKDIR
 
-export ITER=1000000
-export OMP_NUM_THREADS=4
-export OMP_PLACES=threads # threads run on physical cores
+export ITER=500000
+export OMP_NUM_THREADS=1
+#export OMP_PLACES=threads # threads run on physical cores
 
 module load mpich-3.2
 
 # Run the executable in the current directory
-mpiexec -n 64 ./benchmarking 2 1 4 2.5 6 0.75
+mpiexec -n 1 ./benchmarking 2 1 4 2.5 6 0.75
 
 # For compilation:
 # mpicc -g -Wall -fopenmp -o executable source_code.c -lm -std=c99
