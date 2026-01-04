8fb303879d8e01dd54cd279035c35b160307ef25
Volani <gabriele.volani@hpc-head-n2.unitn.it>
2026-01-04 09:40:38 +0100

    good scatterexcl tries

--- definitive_files/benchmarking/benchmarking.sh
+++ definitive_files/benchmarking/benchmarking.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#PBS -l select=8:ncpus=4:mem=2gb -l place=scatter:excl
-#PBS -l walltime=0:30:00
-#PBS -q short_cpuQ
+#PBS -l select=32:ncpus=4:mem=2gb -l place=scatter:excl
+#PBS -l walltime=2:30:00
+#PBS -q short_HPC4DS
 #PBS -V
 
 # Change to the folder where job is submitted (where the executable is located)
 cd $PBS_O_WORKDIR
 
-export ITER=500000
+export ITER=8000000
 export OMP_NUM_THREADS=4
 module load mpich-3.2
 
 # Run the executable in the current directory
-mpirun.actual -n 8 ./benchmarking 2 1 4 2.5 6 0.75
+mpirun.actual -n 32 ../benchmarking 2 1 4 2.5 6 0.75
 #perf stat -e cache-misses,cache-references,cycles,instructions mpiexec -n 1 ./benchmarking 2 1 4 2.5 6 0.75
 
 # For compilation:
-# mpicc -Wall -O2 -fno-tree-vectorize -fopenmp -o executable source_code.c -lm -std=c99
+# mpicc -Wall -fopenmp -o executable source_code.c -lm -std=c99
 
 # To use other PBS strategies:
 # -l place=pack:excl
